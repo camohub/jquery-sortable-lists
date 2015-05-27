f87e4767210a79b46a685ca2553d6b8d7b0a2694
Camo <vladimir.camaj@gmail.com>
2015-05-27 02:45:39 +0200

    Refactoring

--- jquery-sortable-lists.js
+++ jquery-sortable-lists.js
@@ -62,27 +62,27 @@
 
 			setting = $.extend(true, {}, defaults, options),
 
-		// base element from which is counted position of draged element
+			// base element from which is counted position of draged element
 			base = $('<' + setting.listSelector + ' />')
 				.prependTo(jQBody)
 				.attr('id', 'sortableListsBase')
 				.css( setting.baseCss )
 				.addClass( setting.baseClass ),
 
-		// placeholder != state.placeholderNode
-		// placeholder is document fragment and state.placeholderNode is document node
+			// placeholder != state.placeholderNode
+			// placeholder is document fragment and state.placeholderNode is document node
 			placeholder = $('<li />')
 				.attr('id', 'sortableListsPlaceholder')
 				.css( setting.placeholderCss )
 				.addClass( setting.placeholderClass ),
 
-		// hint is document fragment
+			// hint is document fragment
 			hint = $('<li />')
 				.attr('id', 'sortableListsHint')
 				.css( setting.hintCss )
 				.addClass( setting.hintClass ),
 
-		// Is document fragment used as wrapper if hint is inserted to the empty li
+			// Is document fragment used as wrapper if hint is inserted to the empty li
 			hintWrapper = $('<' + setting.listSelector + ' />')
 				.attr('id', 'sortableListsHintWrapper')
 				.addClass( setting.listsClass + ' ' + setting.hintWrapperClass )
@@ -104,7 +104,7 @@
 					return false; // Prevent default
 				}),
 
-		// Container with all actual elements and parameters
+			// Container with all actual elements and parameters
 			state = {
 				isRelEFP: null,  // How browser counts elementFromPoint() position (relative to window/document)
 				oEl: null, // overElement is element which returns elementFromPoint() method
@@ -200,7 +200,7 @@
 				'top': elXY.top - elMT,
 				'left': elXY.left - elML
 			})
-				.prependTo(base);
+			.prependTo(base);
 
 			placeholderNode.css({'display': 'block',
 				'height': elIH
